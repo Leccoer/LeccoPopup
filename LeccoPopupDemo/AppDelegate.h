40827132681351648ac4a1ac6de790d4fcdac7bb
lecco <10915819@qq.com>
2018-12-07 11:41:47 +0800

    Initial Commit

---
+++ LeccoPopupDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeccoPopupDemo
+//
+//  Created by yixin on 2018/12/7.
+//  Copyright © 2018 yixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeccoPopupDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeccoPopupDemo
+//
+//  Created by yixin on 2018/12/7.
+//  Copyright © 2018 yixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
